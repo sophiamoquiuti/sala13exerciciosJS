aa8c2cc3f9d2b8c29a5109cda386d1ffbd260150
sophiamoquiuti <sophiamoquiutitds@gmail.com>
2026-04-29 08:42:39 -0300

    Add files via upload

---
+++ calculos/script.js
@@ -0,0 +1,7 @@
+function somar(){
+    var numero1 = document.getElementById("n1").valueAsNumber
+    var numero2 = document.getElementById("n2").valueAsNumber
+    var res = document.getElementById("res")
+    res.textContent = numero1 + numero2
+
+}
