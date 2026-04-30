51a587255b985d1cac87dfd4964ae85a60fa2ef1
sophiamoquiuti <sophiamoquiutitds@gmail.com>
2026-04-30 09:32:25 -0300

    Add files via upload

--- calculos/script.js
+++ calculos/script.js
@@ -5,3 +5,19 @@ function somar(){
     res.textContent = numero1 + numero2
 
 }
+
+
+function dividir(){
+var n3 = parseFloat(document.getElementById("n3").value)
+var n4 = parseFloat(document.getElementById("n4").value)
+var resp = document.getElementById("resp")
+
+if(n4 !== 0 ){
+    resp.textContent = n3/n4
+}else{
+    resp.textContent = "Não se divide por 0"
+
+}
+
+
+}
